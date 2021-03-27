a4e6fac4599d7d87a64e7f8304019a1a426373bd
Joshhortt <joshhortt@yahoo.com>
2021-03-27 19:22:40 +0000

    Add to class 'Category' a 'Name' and 'DisplayOrder' properties. Add also 'ConnectrionStrings' to 'appsettings.json'.

--- MvcOne/MvcOne/Models/Category.cs
+++ MvcOne/MvcOne/Models/Category.cs
@@ -10,5 +10,8 @@ namespace MvcOne.Models
 	{
 		[Key]  // Add data annotations for database primary key
 		public int Id { get; set; }
+		public string Name { get; set; }
+		public int DisplayOrder{ get; set; }
+
 	}
 }
