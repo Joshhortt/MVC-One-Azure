b3673c1ccdb756794463d914586a6626181795bb
Joshhortt <joshhortt@yahoo.com>
2021-03-27 18:48:57 +0000

    Add first commit of project 'MVC ONE AZURE'. Add class 'Category' to 'Models' and property Id as primary key.

---
+++ MvcOne/MvcOne/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcOne.Models
+{
+	public class Category
+	{
+		[Key]  // Add data annotations for database primary key
+		public int Id { get; set; }
+	}
+}
