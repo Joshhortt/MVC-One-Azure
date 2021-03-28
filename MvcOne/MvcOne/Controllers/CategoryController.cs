239a569e41ae8764193b37e34a103fec91a6a9e8
Joshhortt <joshhortt@yahoo.com>
2021-03-28 15:33:09 +0100

    Create new page to add new Category by creating new action method & adding new view. Add html structure with boostrap classes and tag helpers to implement page UI functionality.

--- MvcOne/MvcOne/Controllers/CategoryController.cs
+++ MvcOne/MvcOne/Controllers/CategoryController.cs
@@ -17,11 +17,30 @@ namespace MvcOne.Controllers
 			_db = db;
 		}
 
-
 		public IActionResult Index()
 		{
 			IEnumerable<Category> objList = _db.Category;
 			return View(objList);
+		}
+
+		// GET - CREATE
+		public IActionResult Create()
+		{
+			return View();  // we don't need to pass anything to the view. Can be empty.
+		}
+
+		//POST - CREATE
+		[HttpPost] // Attribute of POST
+		[ValidateAntiForgeryToken]  // add built-in anti-foregery token 
+		public IActionResult Create(Category obj)
+		{  // object to add to the db
+			if (ModelState.IsValid)
+			{
+				_db.Category.Add(obj);
+				_db.SaveChanges();  // save changes method in the Sql db 
+				return RedirectToAction("Index");  // return to the action method
+			}
+			return View(obj);
 		}
 	}
 }
--- MvcOne/MvcOne/Models/Category.cs
+++ MvcOne/MvcOne/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +12,8 @@ namespace MvcOne.Models
 		[Key]  // Add data annotations for database primary key
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public int DisplayOrder{ get; set; }
 
+		[DisplayName("Display Order")]  // Data annotations added
+		public int DisplayOrder{ get; set; }
 	}
 }
